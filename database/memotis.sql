ad013db3a76fa4480f8ae9f2546f0251e8a27946
= <irawan_agungnugroho@yahoo.com>
2023-07-21 18:08:32 +0700

    update

--- database/memotis.sql
+++ database/memotis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 21, 2023 at 11:08 AM
+-- Generation Time: Jul 21, 2023 at 01:03 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
