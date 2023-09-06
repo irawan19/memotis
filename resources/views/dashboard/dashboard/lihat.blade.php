f0a006e324c3718af3632ca6977e0605869e921b
= <irawan_agungnugroho@yahoo.com>
2023-09-06 16:27:29 +0700

    update counting

--- resources/views/dashboard/dashboard/lihat.blade.php
+++ resources/views/dashboard/dashboard/lihat.blade.php
@@ -91,11 +91,20 @@
 						@php($ambil_divisis = \App\Models\Master_level_sistem::where('id_level_sistems',Auth::user()->level_sistems_id)
 																			->first())
 						@if(Auth::user()->level_sistems_id == 1 || $ambil_divisis->divisis_id == null)
-							@php($ambil_mom_users = \App\Models\Mom_user::where('status_tugas_id',$status_tugas->id_status_tugas)
+							@php($ambil_mom_users = \App\Models\Mom_user::join('moms','mom_users.moms_id','=','moms.id_moms')
+																		->join('users','mom_users.users_id','=','users.id')
+																		->join('master_level_sistems','users.level_sistems_id','=','master_level_sistems.id_level_sistems')
+																		->leftJoin('master_divisis','divisis_id','=','master_divisis.id_divisis')
+																		->whereRaw('moms.id_moms IN (SELECT max(id_moms) FROM moms JOIN mom_users ON mom_users.moms_id=moms.id_moms WHERE status_tugas_id = '.$status_tugas->id_status_tugas.' GROUP BY tugas_mom_users)')
+																		->where('status_tugas_id',$status_tugas->id_status_tugas)
+																		->orderBy('moms.created_at','desc')
 																		->count())
 						@else
-							@php($ambil_mom_users = \App\Models\Mom_user::where('status_tugas_id',$status_tugas->id_status_tugas)
-																		->where('users_id',Auth::user()->id)
+							@php($ambil_mom_users = \App\Models\Mom_user::join('moms','mom_users.moms_id','=','moms.id_moms')
+																		->whereRaw('moms.id_moms IN (SELECT max(id_moms) FROM moms JOIN mom_users ON mom_users.moms_id=moms.id_moms WHERE status_tugas_id = '.$status_tugas->id_status_tugas.' AND mom_users.users_id = '.Auth::user()->id.' GROUP BY tugas_mom_users)')
+																		->where('status_tugas_id',$status_tugas->id_status_tugas)
+																		->where('mom_users.users_id',Auth::user()->id)
+																		->orderBy('moms.created_at','desc')
 																		->count())
 						@endif
 						<div class="col-sm-4">
