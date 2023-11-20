4ed2427db4c88aaff8dba7f2f2ac281c421db282
Cydia <cydia@Cydias-MacBook-Pro.local>
2023-11-20 16:47:57 +0700

    update karyawan excel

--- resources/views/dashboard/karyawan/cetakexcel.blade.php
+++ resources/views/dashboard/karyawan/cetakexcel.blade.php
@@ -63,101 +63,116 @@
     </thead>
     <tbody>
         @if(!$lihat_karyawans->isEmpty())
-        @php($no = 1)
-        @foreach($lihat_karyawans as $karyawans)
-        @if(!empty($karyawans->tanggal_keluar_karyawans))
-        @if(strtotime($karyawans->tanggal_keluar_karyawans) < strtotime(date('Y-m-d'))) @php($color='color:red' ) @else @php($color='' ) @endif @else @php($color='' ) @endif @php($no=1)
-            <tr {{$color}}>
-                <td>{{$no}}</td>
-                <td>
+            @php($no = 1)
+                @foreach($lihat_karyawans as $karyawans)
                     @if(!empty($karyawans->tanggal_keluar_karyawans))
-                    @if(strtotime($karyawans->tanggal_keluar_karyawans) < strtotime(date('Y-m-d'))) Tidak Aktif @else Aktif @endif @else Aktif @endif </td>
-                <td>
-                    <a href="{{URL::asset('storage/'.$karyawans->foto_karyawans)}}">Klik</a>
-                </td>
-                <td>{{$karyawans->nama_karyawans}}</td>
-                <td>{{$karyawans->nama_jabatans}}</td>
-                <td>{{$karyawans->nama_unit_kerjas}}</td>
-                <td>{!! nl2br($karyawans->lokasi_unit_kerjas) !!}</td>
-                <td>'{{$karyawans->nik_gys_karyawans}}</td>
-                <td>'{{$karyawans->nik_tg_karyawans}}</td>
-                <td>
-                    @if(!empty($karyawans->tanggal_bergabung_karyawans))
-                    {{General::ubahDBKeTanggal($karyawans->tanggal_bergabung_karyawans)}}
+                        @if(strtotime($karyawans->tanggal_keluar_karyawans) < strtotime(date('Y-m-d')))
+                            @php($color = 'color:red')
+                        @else
+                            @php($color = '')
+                        @endif
                     @else
-                    -
+                        @php($color = '')
                     @endif
-                </td>
-                <td>{{$karyawans->band_posisi_karyawans}}</td>
-                <td>
-                    @if(!empty($karyawans->tanggal_keluar_karyawans))
-                    {{General::ubahDBKeTanggal($karyawans->tanggal_keluar_karyawans)}}
-                    @else
-                    -
-                    @endif
-                </td>
-                <td>{{$karyawans->nama_status_karyawans}}</td>
-                <td>'{{$karyawans->npwp_karyawans}}</td>
-                <td>'{{$karyawans->ktp_karyawans}}</td>
-                <td>
-                    @if(!empty($karyawans->tanggal_lahir_karyawans))
-                    {{General::ubaHDBkeTanggal($karyawans->tanggal_lahir_karyawans)}}
-                    @else
-                    -
-                    @endif
-                </td>
-                <td>{{$karyawans->tempat_lahir_karyawans}}</td>
-                <td>{{$karyawans->nama_jenis_kelamins}}</td>
-                <td>{{$karyawans->nama_agamas}}</td>
-                <td>{!! nl2br($karyawans->alamat_domisili_karyawans) !!}</td>
-                <td>{{$karyawans->nama_status_kawins}}</td>
-                <td>{{$karyawans->nama_pendidikans}}</td>
-                <td>{{$karyawans->institusi_karyawans}}</td>
-                <td>{!! nl2br($karyawans->hobi_karyawans) !!}</td>
-                <td>{!! nl2br($karyawans->keahlian_khusus_karyawans) !!}</td>
-                <td>
-                    <a href="tel:{{$karyawans->no_hp_karyawans}}">
-                        {{$karyawans->no_hp_karyawans}}
-                    </a>
-                </td>
-                <td>
-                    <a href="mailto:{{$karyawans->email_karyawans}}">
-                        {{$karyawans->email_karyawans}}
-                    </a>
-                </td>
-            </tr>
-            @php($no++)
-            @endforeach
+                    <tr {{$color}}>
+                        <td>{{$no}}</td>
+                        <td>
+                            @if(!empty($karyawans->tanggal_keluar_karyawans))
+                                @if(strtotime($karyawans->tanggal_keluar_karyawans) < strtotime(date('Y-m-d')))
+                                    Tidak Aktif
+                                @else
+                                    Aktif
+                                @endif
+                            @else
+                                Aktif
+                            @endif
+                        </td>
+                        <td>
+                            <a href="{{URL::asset('storage/'.$karyawans->foto_karyawans)}}">Klik</a>
+                        </td>
+                        <td>{{$karyawans->nama_karyawans}}</td>
+                        <td>{{$karyawans->nama_jabatans}}</td>
+                        <td>{{$karyawans->nama_unit_kerjas}}</td>
+                        <td>{!! nl2br($karyawans->lokasi_unit_kerjas) !!}</td>
+                        <td>'{{$karyawans->nik_gys_karyawans}}</td>
+                        <td>'{{$karyawans->nik_tg_karyawans}}</td>
+                        <td>
+                            @if(!empty($karyawans->tanggal_bergabung_karyawans))
+                            {{General::ubahDBKeTanggal($karyawans->tanggal_bergabung_karyawans)}}
+                            @else
+                            -
+                            @endif
+                        </td>
+                        <td>{{$karyawans->band_posisi_karyawans}}</td>
+                        <td>
+                            @if(!empty($karyawans->tanggal_keluar_karyawans))
+                            {{General::ubahDBKeTanggal($karyawans->tanggal_keluar_karyawans)}}
+                            @else
+                            -
+                            @endif
+                        </td>
+                        <td>{{$karyawans->nama_status_karyawans}}</td>
+                        <td>'{{$karyawans->npwp_karyawans}}</td>
+                        <td>'{{$karyawans->ktp_karyawans}}</td>
+                        <td>
+                            @if(!empty($karyawans->tanggal_lahir_karyawans))
+                                {{General::ubaHDBkeTanggal($karyawans->tanggal_lahir_karyawans)}}
+                            @else
+                            -
+                            @endif
+                        </td>
+                        <td>{{$karyawans->tempat_lahir_karyawans}}</td>
+                        <td>{{$karyawans->nama_jenis_kelamins}}</td>
+                        <td>{{$karyawans->nama_agamas}}</td>
+                        <td>{!! nl2br($karyawans->alamat_domisili_karyawans) !!}</td>
+                        <td>{{$karyawans->nama_status_kawins}}</td>
+                        <td>{{$karyawans->nama_pendidikans}}</td>
+                        <td>{{$karyawans->institusi_karyawans}}</td>
+                        <td>{{ $karyawans->hobi_karyawans }}</td>
+                        <td>{{ $karyawans->keahlian_khusus_karyawans }}</td>
+                        <td>
+                            <a href="tel:{{$karyawans->no_hp_karyawans}}">
+                                {{$karyawans->no_hp_karyawans}}
+                            </a>
+                        </td>
+                        <td>
+                            <a href="mailto:{{$karyawans->email_karyawans}}">
+                                {{$karyawans->email_karyawans}}
+                            </a>
+                        </td>
+                    </tr>
+                    @php($no++)
+                @endforeach
             @else
-            <tr>
-                <td colspan="27" align="right-align">Tidak ada data ditampilkan</td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-                <td style="display:none"></td>
-            </tr>
+                <tr>
+                    <td colspan="27" align="right-align">Tidak ada data ditampilkan</td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                    <td style="display:none"></td>
+                </tr>
             @endif
     </tbody>
 </table>
